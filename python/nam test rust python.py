2d02339a7052001cc7be89fcf241cb049375a073
Michael Jancsy <michaeljancsy@gmail.com>
2026-03-31 23:16:25 -0700

    Update python/nam test rust python.py

---
+++ python/nam test rust python.py
@@ -0,0 +1,20 @@
+from conjuredsp.nam import load_model
+from conjuredsp import db, mix
+
+PARAMS = {
+    "input_gain": db(default=0),
+    "output_level": db(default=0),
+    "mix": mix(default=1.0),
+}
+
+model = load_model("tone3000://34/82524")
+
+def process(inputs, outputs, frame_count, sample_rate, params):
+    in_gain = 10 ** (params["input_gain"] / 20.0)
+    out_level = 10 ** (params["output_level"] / 20.0)
+    mix_val = params["mix"]
+
+    for ch in range(len(inputs)):
+        dry = inputs[ch][:frame_count]
+        wet = model.process(dry * in_gain, ch) * out_level
+        outputs[ch][:frame_count] = dry * (1 - mix_val) + wet * mix_val
